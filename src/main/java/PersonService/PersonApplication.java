5c4736d24f59508bc44f53dca8a55edcec9a437c
Кечин Дмитрий <kechin.d@gmail.com>
2022-12-12 03:33:44 +0300

    add gateway and eureka

--- src/main/java/PersonService/PersonApplication.java
+++ src/main/java/PersonService/PersonApplication.java
@@ -1,19 +1,15 @@
 package PersonService;
 
-import PersonService.feighnClient.AuthClient;
-import lombok.RequiredArgsConstructor;
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import javax.annotation.PostConstruct;
-
-@SpringBootApplication
-@EnableFeignClients (clients = {
-		AuthClient.class})
 @EnableCaching
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
 public class PersonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApplication.class, args);
--- src/main/java/PersonService/feighnClient/AuthClient.java
+++ src/main/java/PersonService/feighnClient/AuthClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "authClient", url = "http://localhost:8081/api/v1/auth")
+@FeignClient("auth-service/api/v1/auth")
 public interface AuthClient {
     @PostMapping("/login")
      String login(@RequestBody LoginRequest request);
