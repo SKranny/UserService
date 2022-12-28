00fb966e6f5b4d35e4edb64f045e73f5630296ad
Mullerland <82908656+Mullerland@users.noreply.github.com>
2022-12-28 20:45:37 +0300

    Changed for test

--- src/test/java/PersonService/PersonMapperTest.java
+++ src/test/java/PersonService/PersonMapperTest.java
@@ -2,68 +2,69 @@ package PersonService;
 
 
 import PersonService.mappers.PersonMapper;
-import PersonService.mappers.RoleSetMapper;
 import PersonService.model.Person;
-import PersonService.model.Role;
 import constants.MessagesPermission;
-import constants.RoleType;
 import constants.StatusCode;
 import dto.userDto.PersonDTO;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.mapstruct.Context;
 import org.mapstruct.factory.Mappers;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 @SpringBootTest
 public class PersonMapperTest {
+    private Person person;
+    private PersonDTO personDTO;
 
 
-    private PersonMapper personMapper = Mappers.getMapper(PersonMapper.class);
-
+    private PersonMapper personMapper;
     @Autowired
-    private RoleSetMapper roleSetMapper;
-    Person person = Person.builder()
-            .id(1L)
-            .email("some@mail.com")
-            .phone("+79050534533")
-            .photo("someurl.com")
-            .about("I like to drink coffee")
-            .statusCode(StatusCode.NONE)
-            .roles(roleSetMapper.toRoleSet(Collections.singleton(RoleType.ROLE_USER)))
-            .firstName("Evgeny")
-            .lastName("Smirnov")
-            .birthDay(LocalDate.of(1995, 12, 19))
-            .messagesPermission(MessagesPermission.ALL)
-            .lastOnlineTime(LocalDateTime.of(2022, 12, 05, 22, 35))
-            .build();
+    public void setPersonMapper(PersonMapper personMapper) {
+        this.personMapper = personMapper;
+    }
+
+    @BeforeEach
+    public void init(){
 
 
-    PersonDTO personDTO = PersonDTO.builder()
-            .id(1L)
-            .email("some@mail.com")
-            .phone("+79050534533")
-            .photo("someurl.com")
-            .about("I like to drink coffee")
-            .statusCode(StatusCode.NONE)
-            .firstName("Evgeny")
-            .lastName("Smirnov")
-            .birthDay(LocalDate.of(1995, 12, 19))
-            .messagesPermission(MessagesPermission.ALL)
-            .lastOnlineTime(LocalDateTime.of(2022, 12, 05, 22, 35))
-            .build();
+         person = Person.builder()
+                .id(1L)
+                .email("some@mail.com")
+                .phone("+79050534533")
+                .photo("someurl.com")
+                .about("I like to drink coffee")
+                .firstName("Evgeny")
+                .lastName("Smirnov")
+                .birthDay(LocalDate.of(1995, 12, 19))
+                .messagesPermission(MessagesPermission.ALL)
+                .lastOnlineTime(LocalDateTime.of(2022, 12, 05, 22, 35))
+                .build();
+
+
+         personDTO = PersonDTO.builder()
+                .id(1L)
+                .email("some@mail.com")
+                .phone("+79050534533")
+                .photo("someurl.com")
+                .about("I like to drink coffee")
+                .statusCode(StatusCode.NONE)
+                .firstName("Evgeny")
+                .lastName("Smirnov")
+                .birthDay(LocalDate.of(1995, 12, 19))
+                .messagesPermission(MessagesPermission.ALL)
+                .lastOnlineTime(LocalDateTime.of(2022, 12, 05, 22, 35))
+                .build();
+
+
+    }
+
 
 
     @Test
@@ -71,7 +72,7 @@ public class PersonMapperTest {
 
 
 
-        PersonDTO personDTO = personMapper.toPersonDTO(person);
+         personDTO = personMapper.toPersonDTO(person);
 
         Assertions.assertNotNull(personDTO);
         Assertions.assertEquals(person.getId(), personDTO.getId());
@@ -91,7 +92,7 @@ public class PersonMapperTest {
     @Test
     public void mapDtoToPersonTest() {
 
-        Person person = personMapper.toPerson(personDTO);
+         person = personMapper.toPerson(personDTO);
 
         Assertions.assertNotNull(personDTO);
         Assertions.assertEquals(personDTO.getId(), person.getId());
