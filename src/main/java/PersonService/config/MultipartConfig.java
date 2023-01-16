eca3b3cb7485e9130da553e3062dd37f650f0ea8
Дмитрий Кечин <kechin.d@gmail.com>
2023-01-16 20:42:33 +0300

    add size to MultipartFile

---
+++ src/main/java/PersonService/config/MultipartConfig.java
@@ -0,0 +1,19 @@
+package PersonService.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class MultipartConfig {
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(1024));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(1024));
+        return factory.createMultipartConfig();
+    }
+}
