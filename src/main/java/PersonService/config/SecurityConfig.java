b8c47cdda3a04903269dfb929614d647d4ebb4fb
Oleg Shmelev <o.shmelev76@gmail.com>
2023-01-17 21:09:16 +0300

    just for test

--- src/main/java/PersonService/config/SecurityConfig.java
+++ src/main/java/PersonService/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-
+// hello
         return http
             .csrf().disable()
             .authorizeHttpRequests()
