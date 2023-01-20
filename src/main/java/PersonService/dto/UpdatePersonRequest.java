8a1b778a33eae2cea4a2a108f8a4099bc034b3d6
nikod <nikodim.curo4kin@yandex.ru>
2023-01-20 19:24:15 +0300

    Validation added.

--- src/main/java/PersonService/dto/UpdatePersonRequest.java
+++ src/main/java/PersonService/dto/UpdatePersonRequest.java
@@ -1,21 +1,32 @@
 package PersonService.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class UpdatePersonRequest {
-
+    @NotBlank
     String firstName;
 
+    @NotBlank
     String lastName;
 
+    @NotBlank
     String birthDate;
 
+    @NotBlank
     String phone;
 
+    @NotBlank
     String about;
 
+    @NotBlank
+    @JsonProperty(required = true)
     String country;
 
+    @NotBlank
+    @JsonProperty(required = true)
     String city;
 }
